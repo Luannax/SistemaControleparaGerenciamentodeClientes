0437424db7f2a9b104190cf37ed96b6f7cc98303
Luanna <luannabahia1@gmail.com>
2023-09-13 10:19:07 -0400

    first commit

---
+++ login/cadastro/js/login.js
@@ -0,0 +1,21 @@
+document.addEventListener("DOMContentLoaded", function() {
+    var entrarBtn = document.getElementById("entrarBtn");
+    var loading = document.getElementById("loading");
+
+    entrarBtn.addEventListener("click", function(event) {
+        event.preventDefault(); // Impede o envio do formulário
+
+        // Esconde o botão "Entrar" e exibe o elemento de carregamento
+        entrarBtn.classList.add("hidden");
+        loading.style.display = "block";
+
+        // Simula o carregamento durante 2 segundos (ajuste conforme necessário)
+        setTimeout(function() {
+            // Redireciona o usuário para a página de destino após o carregamento
+            window.location.href = "https://cadastro-jade.vercel.app/";
+
+            // Remove a classe hidden para exibir o botão novamente
+            entrarBtn.classList.remove("hidden");
+        }, 2000); // 2 segundos de atraso
+    });
+});
